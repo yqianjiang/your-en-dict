5a253d4514a89f2321df3e0bdfaaa8d0f79ea5e4
yqianjiang <y.jiang.psy@gmail.com>
2022-08-11 02:04:58 +0800

    feat: naive ui全局主题配置

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script setup>
-
+import NaiveTheme from "@/components/NaiveTheme.vue";
 </script>
 
 <template>
   <a href="#/reading">Reading</a> |
+  <a href="#/my-reading">My Reading</a> |
   <a href="#/me">Me</a>
-  <router-view v-slot="{ Component, route }">
-    <!-- <transition :name="route.meta.transition"> -->
-      <!-- <KeepAlive exclude="article"> -->
-        <component :is="Component" />
-      <!-- </KeepAlive> -->
-    <!-- </transition> -->
-  </router-view>
+  <NaiveTheme>
+    <router-view v-slot="{ Component, route }">
+      <!-- <transition :name="route.meta.transition"> -->
+        <!-- <KeepAlive exclude="article"> -->
+          <component :is="Component" />
+        <!-- </KeepAlive> -->
+      <!-- </transition> -->
+    </router-view>
+  </NaiveTheme>
 </template>
 
 <style scoped>
