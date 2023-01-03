b146e6c156ce11d6b1e6427473cbd790544af349
yqianjiang <y.jiang.psy@gmail.com>
2023-01-03 23:38:46 +0800

    refactor: 重新组织article页的代码

--- src/views/reading/article.vue
+++ src/views/reading/article.vue
@@ -10,70 +10,88 @@ import { compare } from "@/utils/articles/counter.js";
 import { userDict } from "@/utils/dict/userDict.js";
 import { getTranslation, getTranslationBatch } from "@/services/dict.js";
 
-const route = useRoute();
+const useData = () => {
+  const data = reactive({
+    knownWord: [],
+    unseenWord: [],
+    unknownWord: [],
+    translations: {},
+  });
+
+  const updateUserDict = () => {
+    userDict.addKnownWords([...data.knownWord, ...data.unseenWord]);
+    userDict.addUnknownWords(data.unknownWord);
+    userDict.save();
+    data.unseenWord = [];
+
+    // console.log(data);
+    // console.log(userDict.unknownWords);
+    // console.log(userDict.knownWords);
+  };
+
+  function onRemove(type, word) {
+    if (type === "unknown") {
+      data.knownWord.push(word);
+      data.unknownWord = data.unknownWord.filter((x) => x !== word);
+      // 熟词不用显示翻译了
+      delete data.translations[word];
+    } else if (type === "known") {
+      data.unknownWord.push(word);
+      data.knownWord = data.knownWord.filter((x) => x !== word);
+      // 生词需要增加翻译
+      _findWordTrans(word);
+    } else {
+      // unseen, 暂时移到生词...
+      data.unknownWord.push(word);
+      data.unseenWord = data.unseenWord.filter((x) => x !== word);
+    }
+  }
+
+  const toggleTrans = (token) => {
+    if (token in data.translations) {
+      delete data.translations[token];
+    } else {
+      _findWordTrans(token);
+    }
+  };
+
+  const _findWordTrans = async (words) => {
+    if (typeof words === "string") {
+      const trans = await getTranslation(words);
+      data.translations[words] = trans;
+      console.log(trans);
+    } else {
+      const trans = await getTranslationBatch({ words });
+      Object.assign(data.translations, trans);
+      console.log(trans);
+    }
+  };
+
+  onBeforeUnmount(() => {
+    // userDict.addSeenWords(article.value.wordsFreq);
+    userDict.save();
+  });
+
+  return { data, toggleTrans, updateUserDict, onRemove };
+};
+
+const { data, toggleTrans, updateUserDict, onRemove } = useData();
 const article = ref({});
-const data = reactive({
-  knownWord: [],
-  unseenWord: [],
-  unknownWord: [],
-  translations: {},
-});
-
-const toggleTrans = (token) => {
-  if (token in data.translations) {
-    delete data.translations[token];
-  } else {
-    _findWordTrans(token);
-  }
-};
-
-const _findWordTrans = async (words) => {
-  if (typeof words === "string") {
-    const trans = await getTranslation(words);
-    data.translations[words] = trans;
-    console.log(trans);
-  } else {
-    const trans = await getTranslationBatch({ words });
-    Object.assign(data.translations, trans);
-    console.log(trans);
-  }
-};
+const route = useRoute();
 
 const onUpdateUserDict = () => {
-  userDict.addKnownWords([...data.knownWord, ...data.unseenWord]);
-  userDict.addUnknownWords(data.unknownWord);
-  userDict.save();
+  updateUserDict();
   articlesHelper.markArticle("hasMarked", article.value);
-  data.unseenWord = [];
-  console.log(data);
-  console.log(userDict.unknownWords);
-  console.log(userDict.knownWords);
-};
-
-// bug: 移动后不影响高亮？
-function onRemove(type, word) {
-  if (type === "unknown") {
-    data.knownWord.push(word);
-    data.unknownWord = data.unknownWord.filter((x) => x !== word);
-    // 熟词不用显示翻译了
-    delete data.translations[word];
-  } else if (type === "known") {
-    data.unknownWord.push(word);
-    data.knownWord = data.knownWord.filter((x) => x !== word);
-    // 生词需要增加翻译
-    _findWordTrans(word);
-  } else {
-    // unseen, 暂时移到生词...
-    data.unknownWord.push(word);
-    data.unseenWord = data.unseenWord.filter((x) => x !== word);
-  }
 }
 
 onMounted(async () => {
+  // 获取article
   const uuid = route.params.id;
   const result = await articlesHelper.getArticle(uuid);
   article.value = result;
   document.title = result.title + " - 你的定制英语词典";
+
+  // 更新data
   const { known, unknown, unseen } = compare(
     article.value.wordsUnique,
     userDict.knownWords,
@@ -82,15 +100,9 @@ onMounted(async () => {
   data.unknownWord = unknown;
   data.unseenWord = unseen;
   data.knownWord = known;
+  toggleTrans([...data.unknownWord, ...data.unseenWord]);
 
   articlesHelper.markArticle("hasOpened", article.value);
-
-  _findWordTrans([...data.unknownWord, ...data.unseenWord]);
-});
-
-onBeforeUnmount(() => {
-  // userDict.addSeenWords(article.value.wordsFreq);
-  userDict.save();
 });
 </script>
 
@@ -105,29 +117,3 @@ onBeforeUnmount(() => {
     <WordListPopup :data="data" :onRemove="onRemove" />
   </ReadingView>
 </template>
-
-<style scoped>
-.token--unseen {
-  color: gray;
-}
-.token--unknown {
-  color: yellow;
-}
-.article {
-  white-space: pre-wrap;
-  font-size: 1.5em;
-  line-height: 200%;
-}
-.translate-sentence {
-  font-size: 0.75em;
-  margin-bottom: 30px;
-}
-.n-space {
-  align-items: center;
-}
-@media (prefers-color-scheme: light) {
-  .token--unknown {
-    color: blue;
-  }
-}
-</style>
