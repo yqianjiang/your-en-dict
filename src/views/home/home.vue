7b60fc5846e8b7517d13ff1d49cb068b28eb2e21
yqianjiang <y.jiang.psy@gmail.com>
2023-01-05 00:19:11 +0800

    feat: 首页界面修改

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -2,6 +2,7 @@
 import { ref } from "vue";
 import { NSpace, NButton, useMessage } from "naive-ui";
 import ReadingView from "@/components/ReadingView.vue";
+// import ReadingListItem from "@/components/ReadingListItem.vue";
 
 import { userDict } from "@/utils/dict/userDict.js";
 import { articlesHelper } from "@/utils/articles/articles.js";
@@ -37,6 +38,7 @@ function clear() {
 </script>
 
 <template>
+  <p>请输入您想阅读的材料，AI会帮你高亮您的<a href="#/me">生词及目标词</a></p>
   <div v-if="!data" class="article-input-area">
     <textarea cols="30" rows="10" v-model="textareaVal"></textarea>
   </div>
@@ -46,18 +48,36 @@ function clear() {
     <NButton v-show="textareaVal" @click="clear">清空</NButton>
   </NSpace>
   <div class="stats-info" v-if="data">
-    <p>总字数{{ data.totalWords }}词</p>
     <p>
-      词汇量{{ data.totalUniqueWords }}词(目标{{ data.targetWords.length }})
+      <span>总字数{{ data.totalWords }}词</span>
+      <span>
+        - 词汇量{{ data.totalUniqueWords }}词(目标{{ data.targetWords.length }})
+      </span>
     </p>
-    <p>
+    <span>
       生词{{ data.unknown.length }}词(目标{{ data.unknownTargetWords.length }})
-    </p>
-    <p>
-      未标记词{{ data.unseen.length }}词(目标{{
+    </span>
+    <span>
+      - 未标记词{{ data.unseen.length }}词(目标{{
         data.unseenTargetWords.length
       }})
-    </p>
+    </span>
   </div>
-  <ReadingView v-if="article" :article="article"> </ReadingView>
+  <div class="reading-view-wrapper" v-if="article">
+    <ReadingView :article="article"> </ReadingView>
+  </div>
 </template>
+
+<style scoped>
+.article-input-area > textarea {
+  width: 100%;
+}
+
+.reading-view-wrapper {
+  margin: auto;
+  max-width: 720px;
+  max-height: 70vh;
+  overflow: auto;
+  border: solid 1px gray;
+}
+</style>
